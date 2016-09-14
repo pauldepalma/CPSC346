531d22808f28b7463b24800e8c9dc993488e1e02
pauldepalma <depalma@gonzaga.edu>
2016-09-13 18:15:47 -0700

    fixed myShell

--- myShell.c
+++ myShell.c
@@ -3,6 +3,9 @@
 
 #define MAX_LINE 80
 
+char** getInput();
+void dispOutput(char**);
+
 int main()
 {
   //A pointer to an array of pointers to char.  In essence, an array of 
@@ -15,25 +18,27 @@ int main()
   {
      printf("myShell> ");
      fflush(stdout);
-     args = getInput(args);
-     //if arg[0] holds 'quit', set more to 0, else invoke dispInput()
+     args = getInput();
+     dispOutput(args);
+     //here accept input from the user 
+     //set more to 0 if input == "quit"  (use strcmp)  
   }
    return 0;
 }
 
 
-//Reads input string from the key board.  Parses the input into arguments
-//where any is any string of characters that either begin the input or is
-//separated by the previous argument by a space.  Returns a pointer to an array //of pointers to char.  char* argv[] is the model.
+//Reads input string from the key board.  Parses the input into argument 
+//where an argument is any string of characters that either begins 
+//the input or is
+//separated by the previous argument by a space
 //In the following example, the user has entered three arguments
 //myShell> ls -l cpsc346 
-//and optons  
-char** args getInput()
+char** getInput()
 {
 }  
 
 
 //Displays the arguments entered by the user and parsed by getInput
-void dispInput(char*[])
+void dispOutput(char** args)
 {
 }  
