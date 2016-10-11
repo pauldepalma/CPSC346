be1800b393a588f859e8dfe63469af9639afb8be
pauldepalma <depalma@gonzaga.edu>
2016-10-11 16:40:33 -0700

    added thread scheduling query pgm

--- pgm24.c
+++ pgm24.c
@@ -1,37 +1,62 @@
+//Demonstrates scope of thread scheduling
 
+#include <pthread.h>
 #include <stdio.h>
-#include <sys/resource.h>
-#include <math.h>
+#define NUM_THREADS 5
 
+void* runner(void*);
+void get_scope(pthread_attr_t*); 
 
-int main(void)
+int main()
 {
-  int which = PRIO_PROCESS; //interpret 2nd arg of getpriority as pid 
-  id_t pid;
-  int ret;
-  int ct = 0;
+ int i;
+ pthread_t tid[NUM_THREADS];
+ pthread_attr_t attr;
 
-  pid = getpid();
-  ret = getpriority(which, pid);
-  printf("default priority is:  %d\n", ret);
-  
-  setpriority(which, pid, 15);
-  ret = getpriority(which, pid);
-  printf("set priority to 15:  %d\n", ret);
+ //get default attributes
+ pthread_attr_init(&attr);
 
-  printf("priority set with nice:%d\n", nice());
+ printf("default scope\n");
+ get_scope(&attr); 
 
-  ret = setpriority(which, pid, 0);
-  ret = getpriority(which, pid);
-  printf("priority reset to default:  %d\n", ret);
-  printf("Notice that it can't be done\n");
+ //set scheduler to process scope or system scope
+ //pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+ pthread_attr_setscope(&attr, PTHREAD_SCOPE_PROCESS); 
 
-  return 0; 
-}     
+ printf("after setting scope\n"); //unix permits only system level scheduling
+ get_scope(&attr); 
 
-void do_stuff() 
+ //create the threads
+ for (i = 0; i < NUM_THREADS; i++)
+   pthread_create(&tid[i], &attr, runner, (void*)i);
+
+ //join on each thread
+ for (i = 0; i < NUM_THREADS; i++)
+    pthread_join(tid[i],  NULL);
+}
+
+
+/*Do some work*/
+void* runner(void* tid)
 {
-  int ct = 0;
-  while (ct++ < 1000)
-    sqrt(ct);
+ printf("Hello from thread %d\n",(int)tid);
+ pthread_exit(NULL);
+} 
+
+/*Get Scope*/
+void get_scope(pthread_attr_t* attr)
+{
+ int scope;
+ if (pthread_attr_getscope(attr,&scope) != 0)
+   fprintf(stderr, "unable to get scope\n");
+ else
+   {
+    if (scope == PTHREAD_SCOPE_PROCESS) //scheduling at user level
+      printf("THREAD_SCOPE_PROCESS\n");
+    else
+      if (scope == PTHREAD_SCOPE_SYSTEM) //scheduling at kernel level
+        printf("THREAD_SCOPE_SYSTEM\n");
+      else
+        fprintf(stderr, "Illegal scope value\n");
+   }
 }
