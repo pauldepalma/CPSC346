0fe274861979af233e5f62c2d7b06003a334b74e
pauldepalma <depalma@gonzaga.edu>
2018-09-11 12:47:18 -0700

    starts

---
+++ starts/proj3.c
@@ -0,0 +1,81 @@
+/*
+Class: CPSC 346-01 & CPSC 346-02
+Team Member 1: Paul De Palma
+Team Member 2: N/A 
+GU Username of project lead: depalma
+Pgm Name: proj3.c 
+Pgm Desc: exploraiton of the proc file system 
+Usage: 1) standard:  ./a.out -s 
+Usage: 2) history:  ./a.out -h 
+Usage: 3) load:  ./a.out -l 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void standard();
+void history();
+void load();
+
+int main(int argc, char* argv[])
+{
+ if (argc != 2)
+  {
+   fprintf(stderr, "Error: Options required\n"); 
+   fprintf(stderr, "usage: ./a.out -s|-h|-l\n\n"); 
+   exit(EXIT_FAILURE);
+  }
+  
+ if (!strcmp(argv[1],"-s"))
+  standard();
+ if (!strcmp(argv[1],"-h"))
+  history();
+ if (!strcmp(argv[1],"-l"))
+  load();
+}
+/*
+pre: none
+post: displays CPU vendor_id, model name, and OS version
+*/
+void standard()
+{
+ char ch;
+ FILE* ifp;
+ char str[80];
+
+ /*
+ I've deliberately used two different mechanisms for writing to the console.  
+ Use whichever suits you.
+ strstr locates a substring
+ */
+
+ ifp = fopen("/proc/cpuinfo","r");
+ while (fgets(str,80,ifp) != NULL)
+  if (strstr(str,"vendor_id") || strstr(str,"model name"))
+   puts(str); 
+ fclose (ifp);
+
+ ifp = fopen("/proc/version","r");
+ while ((ch = getc(ifp)) != EOF)
+  putchar(ch);
+ fclose (ifp);
+}
+
+/*
+pre: none
+post: displays time since the last reboot (DD:HH:MM:SS), time when the system was last booted 
+      (MM/DD/YY - HH:MM), number of processes that have been created since the last reboot.
+Hint: strftime could be useful
+*/
+void history()
+{
+}
+
+/*
+pre: none
+post: displays total memory, available memory, load average (avg. number of processes over the last minute) 
+*/
+void load()
+{
+}
